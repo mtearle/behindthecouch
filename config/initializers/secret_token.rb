1d009d5504228f611319f55fd92dd743faa9e6a9
Mark Tearle <mark@tearle.com>
2013-05-20 21:49:58 +0800

    Initial Rails generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Behindthecouch::Application.config.secret_token = 'c2e79af6abcb22216095fb9b974d22067dc98cba7b982ad2fd766b623336b3126f136dd2fe44582aa031c7510abca73a588daed84c7e4c9cb6bbcb1cb7eb4e64'
